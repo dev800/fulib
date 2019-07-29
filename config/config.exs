e10e0e4bf78795cbf7227e0187b2ed00596d2c40
WangJinzhong <andywang7259@163.com>
2019-07-29 11:48:44 +0800

    config vendor

--- config/config.exs
+++ config/config.exs
@@ -47,3 +47,7 @@ config :fulib, Fulib.Gettext, default_locale: "zh_CN"
 if File.exists?("#{__DIR__}/#{Mix.env()}.exs") do
   import_config "#{Mix.env()}.exs"
 end
+
+if File.exists?("#{__DIR__}/#{Mix.env()}.secret.exs") do
+  import_config "#{Mix.env()}.secret.exs"
+end
